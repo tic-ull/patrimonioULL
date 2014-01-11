01b85dee1ac5fa95a0c0a3161d8c389c0f2e126f
Rayco Abad Martín <rayco.abad@gmail.com>
2014-01-11 13:05:34 +0000

    PEP 8

--- patrimonioULL/urls.py
+++ patrimonioULL/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from django.conf import settings
+from django.conf import settings as st
 from django.conf.urls.static import static
 
-urlpatterns = patterns('', (
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'patrimonioULL.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(st.MEDIA_URL, document_root=st.MEDIA_ROOT)
