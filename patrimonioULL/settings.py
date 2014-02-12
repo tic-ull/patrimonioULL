21f93dcf8eea2505f16fd8d5294b3bb84db80fc1
Rayco Abad Martín <rabadmar@ull.es>
2014-02-12 08:36:16 +0000

    - Added static

--- patrimonioULL/settings.py
+++ patrimonioULL/settings.py
@@ -98,6 +98,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR + '/static/'
 
 MEDIA_ROOT = BASE_DIR + '/media/'
 MEDIA_URL = '/media/'
