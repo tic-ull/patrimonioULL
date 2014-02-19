34988c9d8d45039d75818b128d5b003c598ef9da
Rayco Abad Martín <rabadmar@ull.es>
2014-02-19 08:29:56 +0000

    Reset migrations history (South)

--- patrimonio/migrations/0001_initial.py
+++ patrimonio/migrations/0001_initial.py
@@ -10,35 +10,38 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding model 'DisciplinaArtistica'
         db.create_table(u'patrimonio_disciplinaartistica', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True, db_column='Id')),
-            ('disciplina', self.gf('django.db.models.fields.CharField')(max_length=50, db_column='DISCIPLINAS ARTISTICAS', blank=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('disciplina', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
         ))
         db.send_create_signal(u'patrimonio', ['DisciplinaArtistica'])
 
+        # Adding model 'LocalizacionObra'
+        db.create_table(u'patrimonio_localizacionobra', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('codigo', self.gf('django.db.models.fields.CharField')(unique=True, max_length=10)),
+            ('localizacion', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal(u'patrimonio', ['LocalizacionObra'])
+
         # Adding model 'ObraDeArte'
         db.create_table(u'patrimonio_obradearte', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True, db_column='Id')),
-            ('n_de_registro', self.gf('django.db.models.fields.CharField')(max_length=50, db_column='N DE REGISTRO', blank=True)),
-            ('titulo', self.gf('django.db.models.fields.CharField')(max_length=255, db_column='TITULO', blank=True)),
-            ('autor', self.gf('django.db.models.fields.CharField')(max_length=50, db_column='AUTOR', blank=True)),
-            ('dibujo', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='DIBUJO', blank=True)),
-            ('pintura', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='PINTURA', blank=True)),
-            ('escultura', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='ESCULTURA', blank=True)),
-            ('fotografia', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='FOTOGRAFIA', blank=True)),
-            ('grabado', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='GRABADO', blank=True)),
-            ('ceramica', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='CERAMICA', blank=True)),
-            ('litografia', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='LITOGRAFIA', blank=True)),
-            ('otros', self.gf('django.db.models.fields.NullBooleanField')(default=False, null=True, db_column='OTROS', blank=True)),
-            ('imagen', self.gf('django.db.models.fields.files.ImageField')(max_length=100, db_column='IMAGEN', blank=True)),
-            ('medidas', self.gf('django.db.models.fields.CharField')(max_length=100, db_column='MEDIDAS', blank=True)),
-            ('tematica_y_estilo', self.gf('django.db.models.fields.CharField')(max_length=50, db_column='TEMATICA Y ESTILO', blank=True)),
-            ('tecnica', self.gf('django.db.models.fields.CharField')(max_length=50, db_column='TECNICA', blank=True)),
-            ('fecha', self.gf('django.db.models.fields.CharField')(max_length=100, db_column='FECHA', blank=True)),
-            ('ubicacion', self.gf('django.db.models.fields.TextField')(db_column='UBICACION', blank=True)),
-            ('estado_de_conservacion', self.gf('django.db.models.fields.CharField')(max_length=50, db_column='ESTADO DE CONSERVACION', blank=True)),
-            ('desperfectos', self.gf('django.db.models.fields.TextField')(db_column='DESPERFECTOS', blank=True)),
-            ('contacto', self.gf('django.db.models.fields.TextField')(db_column='CONTACTO', blank=True)),
-            ('observaciones', self.gf('django.db.models.fields.TextField')(db_column='OBSERVACIONES', blank=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('registro', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
+            ('titulo', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('autor', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('disciplina', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['patrimonio.DisciplinaArtistica'], on_delete=models.PROTECT)),
+            ('imagen', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
+            ('imagen_trasera', self.gf('django.db.models.fields.files.ImageField')(max_length=100, blank=True)),
+            ('medidas', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('tematica', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('tecnica', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('fecha', self.gf('django.db.models.fields.CharField')(max_length=100, blank=True)),
+            ('localizacion', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['patrimonio.LocalizacionObra'], on_delete=models.PROTECT)),
+            ('ubicacion', self.gf('tinymce.models.HTMLField')(blank=True)),
+            ('estado', self.gf('django.db.models.fields.CharField')(max_length=50, blank=True)),
+            ('desperfectos', self.gf('tinymce.models.HTMLField')(blank=True)),
+            ('contacto', self.gf('tinymce.models.HTMLField')(blank=True)),
+            ('observaciones', self.gf('tinymce.models.HTMLField')(blank=True)),
         ))
         db.send_create_signal(u'patrimonio', ['ObraDeArte'])
 
@@ -47,40 +50,44 @@ class Migration(SchemaMigration):
         # Deleting model 'DisciplinaArtistica'
         db.delete_table(u'patrimonio_disciplinaartistica')
 
+        # Deleting model 'LocalizacionObra'
+        db.delete_table(u'patrimonio_localizacionobra')
+
         # Deleting model 'ObraDeArte'
         db.delete_table(u'patrimonio_obradearte')
 
 
     models = {
         u'patrimonio.disciplinaartistica': {
-            'Meta': {'object_name': 'DisciplinaArtistica'},
-            'disciplina': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_column': "'DISCIPLINAS ARTISTICAS'", 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True', 'db_column': "'Id'"})
+            'Meta': {'ordering': "['disciplina']", 'object_name': 'DisciplinaArtistica'},
+            'disciplina': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'patrimonio.localizacionobra': {
+            'Meta': {'ordering': "['localizacion']", 'object_name': 'LocalizacionObra'},
+            'codigo': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '10'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'localizacion': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
         u'patrimonio.obradearte': {
             'Meta': {'object_name': 'ObraDeArte'},
-            'autor': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_column': "'AUTOR'", 'blank': 'True'}),
-            'ceramica': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'CERAMICA'", 'blank': 'True'}),
-            'contacto': ('django.db.models.fields.TextField', [], {'db_column': "'CONTACTO'", 'blank': 'True'}),
-            'desperfectos': ('django.db.models.fields.TextField', [], {'db_column': "'DESPERFECTOS'", 'blank': 'True'}),
-            'dibujo': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'DIBUJO'", 'blank': 'True'}),
-            'escultura': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'ESCULTURA'", 'blank': 'True'}),
-            'estado_de_conservacion': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_column': "'ESTADO DE CONSERVACION'", 'blank': 'True'}),
-            'fecha': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_column': "'FECHA'", 'blank': 'True'}),
-            'fotografia': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'FOTOGRAFIA'", 'blank': 'True'}),
-            'grabado': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'GRABADO'", 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True', 'db_column': "'Id'"}),
-            'imagen': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'db_column': "'IMAGEN'", 'blank': 'True'}),
-            'litografia': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'LITOGRAFIA'", 'blank': 'True'}),
-            'medidas': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_column': "'MEDIDAS'", 'blank': 'True'}),
-            'n_de_registro': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_column': "'N DE REGISTRO'", 'blank': 'True'}),
-            'observaciones': ('django.db.models.fields.TextField', [], {'db_column': "'OBSERVACIONES'", 'blank': 'True'}),
-            'otros': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'OTROS'", 'blank': 'True'}),
-            'pintura': ('django.db.models.fields.NullBooleanField', [], {'default': 'False', 'null': 'True', 'db_column': "'PINTURA'", 'blank': 'True'}),
-            'tecnica': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_column': "'TECNICA'", 'blank': 'True'}),
-            'tematica_y_estilo': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_column': "'TEMATICA Y ESTILO'", 'blank': 'True'}),
-            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_column': "'TITULO'", 'blank': 'True'}),
-            'ubicacion': ('django.db.models.fields.TextField', [], {'db_column': "'UBICACION'", 'blank': 'True'})
+            'autor': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'contacto': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'desperfectos': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'disciplina': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patrimonio.DisciplinaArtistica']", 'on_delete': 'models.PROTECT'}),
+            'estado': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'fecha': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'imagen': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'imagen_trasera': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'localizacion': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['patrimonio.LocalizacionObra']", 'on_delete': 'models.PROTECT'}),
+            'medidas': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'observaciones': ('tinymce.models.HTMLField', [], {'blank': 'True'}),
+            'registro': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'tecnica': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'tematica': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'titulo': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'ubicacion': ('tinymce.models.HTMLField', [], {'blank': 'True'})
         }
     }
 
